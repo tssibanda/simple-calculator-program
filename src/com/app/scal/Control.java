1e3572ba2ed9a91099968ac9c65bc6f908aa028a
tssibanda <tssibanda@hotmail.com>
2021-09-30 20:07:39 +0100

    Initial simple calculator code

---
+++ src/com/app/scal/Control.java
@@ -0,0 +1,20 @@
+
+/***************************************************************************	
+ * 	Description:			Control to run the Simple 4 Calculator Program
+ * 
+ * 	Author:				 	C18727971 Thamsanqa Sibanda
+ * 
+ * 	Date:				 	30 September 2021
+ ***************************************************************************/
+
+package com.app.scal;
+
+public class Control {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World! I am the Calculator!");
+
+	}// end main()
+
+}// end Control Class
